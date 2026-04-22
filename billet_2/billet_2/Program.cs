a882a13fbe29aabe3d1819931ff73d64cad5f310
luizp <luizprosa2@gmail.com>
2026-04-21 21:12:56 -0300

    front conversa (deve) com API

--- billet_2/billet_2/Program.cs
+++ billet_2/billet_2/Program.cs
@@ -1,10 +1,22 @@
 using billet_2.Components;
+using billet_2.Services; // Adicionado: Para reconhecer suas pastas de serviço
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveWebAssemblyComponents()
+    .AddInteractiveServerComponents(); // Mantendo o seu suporte a Server Mode
+
+// --- ADICIONADO DO GIT: CONFIGURAÇÃO DA API ---
+builder.Services.AddScoped(dp => new HttpClient
+{
+    BaseAddress = new Uri("http://localhost:5289") // O endereço da API do cara do backend
+});
+
+// --- ADICIONADO DO GIT: REGISTRO DOS SERVIÇOS ---
+builder.Services.AddScoped<EventoService>();
+builder.Services.AddScoped<UsuarioService>();
 
 var app = builder.Build();
 
@@ -16,16 +28,17 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
-    .AddInteractiveWebAssemblyRenderMode();
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddInteractiveServerRenderMode(); // Mantendo o seu suporte a Server Mode
 
 app.Run();
---
+++ billet_2/billet_2/Services/EventoService.cs
@@ -0,0 +1,31 @@
+using System.Net.Http.Json;
+using billet_2.Models;
+
+namespace billet_2.Services;
+
+public class EventoService
+{
+    private readonly HttpClient _http;
+
+    public EventoService(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<List<Evento>?> ListarEventosAsync()
+    {
+        return await _http.GetFromJsonAsync<List<Evento>>("api/eventos/listar");
+    }
+
+    public async Task<Evento?> BuscarPorIdAsync(int id)
+    {
+        return await _http.GetFromJsonAsync<Evento>($"api/eventos/listar/{id}");
+    }
+
+    // ADICIONE ESTE AQUI PARA A SUA PÁGINA DE CRIAR EVENTOS FUNCIONAR
+    public async Task<bool> CriarEventoAsync(Evento novoEvento)
+    {
+        var response = await _http.PostAsJsonAsync("api/eventos/cadastrar", novoEvento);
+        return response.IsSuccessStatusCode;
+    }
+}
