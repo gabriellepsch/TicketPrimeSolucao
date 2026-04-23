c4878629afdf12583a6576cbd67215814e3b293c
Castxr <gabrielcastordeoliveira@gmail.com>
2026-04-23 19:09:59 -0300

    Add files via upload

--- billet_2/billet_2/Models/Usuario.cs
+++ billet_2/billet_2/Models/Usuario.cs
@@ -1,9 +1,10 @@
 namespace billet_2.Models;
 
-public class Usuario{
-    public int Id {get;set;}
-    public string Nome {get;set;} = "";
-    public string Email {get;set;} = "";
-    public string Cpf {get;set;} = "";
-    public string Senha {get;set;} = ""; 
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Cpf { get; set; } = "";
+    public string Senha { get; set; } = "";
 }
