82aee545f47cff42c19006b389cefec474f63410
lepsch <gabriel.lepmont@gmail.com>
2026-04-22 23:28:26 -0300

    Upei a branch

--- billet_2/billet_2/obj/Debug/net10.0/billet_2.AssemblyInfo.cs
+++ billet_2/billet_2/obj/Debug/net10.0/billet_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("billet_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247275defcdbace6ba59d44cd7d8d37810b297e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("billet_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("billet_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
